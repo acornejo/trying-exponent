6493eab9c6c2d55aa8f2d3a6228a4fec6f1e510d
Alex Cornejo <acornejo@gmail.com>
2017-01-14 21:33:23 -0800

    playing with exponent

--- main.js
+++ main.js
@@ -1,7 +1,41 @@
 // @flow
 
 import Exponent from 'exponent';
-
+import React from 'react';
 import App from './src/app';
 
-Exponent.registerRootComponent(App);
+import { View, StatusBar, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  sbar: {
+    height: StatusBar.currentHeight,
+    backgroundColor: '#5067FF'
+  }
+});
+
+class Wrapper extends React.Component {
+  state = { fontsAreLoaded: false };
+
+  async componentWillMount() {
+    await Exponent.Font.loadAsync({
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf')
+    });
+    this.setState({fontsAreLoaded: true});
+  }
+
+  render() {
+    if (this.state.fontsAreLoaded) {
+      return (<View style={styles.container}>
+          <View style={styles.sbar}/>
+          <StatusBar barStyle='default'/>
+          <App/>
+        </View>);
+    } else
+      return <Exponent.Components.AppLoading/>;
+  }
+}
+
+Exponent.registerRootComponent(Wrapper);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 // @flow
 
-import React, { Component } from 'react';
-import { LayoutAnimation, ScrollView, StatusBar, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { Container, Text, Header, Title, Content, Footer, FooterTab, Button, Icon } from 'native-base';
+
+import { TouchableWithoutFeedback, View, Animated, Easing, StyleSheet } from 'react-native';
+import Ionicon from 'react-native-vector-icons/Ionicons';
 
 import { action, observable } from 'mobx';
 import { observer } from 'mobx-react/native';
@@ -18,35 +21,137 @@ class Store {
 
 const store = new Store();
 
-@observer
-class App extends Component {
-  componentDidMount() {
-    StatusBar.setHidden(true);
+const border = 1;
+const gap = 2;
+const duration = 100;
+const size = 30;
+const bg = '#5067FF';
+const fg = '#FFFFFF';
+
+const styles = StyleSheet.create({
+  circle: {
+    width: size,
+    height: size,
+    borderRadius: size/2,
+    borderWidth: border,
+    borderColor: bg,
+    backgroundColor: fg,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  dot: {
+    backgroundColor: bg
+  },
+  checkmark: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: fg,
+    height: size-2*border,
+    fontSize: size-2*border
+  }
+});
+
+class Checkbox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: props.selected,
+      opacity: new Animated.Value(props.selected ? 100 : 0)
+    };
   }
 
-  componentWillUpdate() {
-    LayoutAnimation.spring();
+  toggle = () => {
+    this.setState({selected: !this.state.selected});
+    Animated.timing(this.state.opacity, {
+      toValue: this.state.selected ? 100 : 0,
+      duration: duration
+    }).start();
+  }
+
+  render() {
+    const color = this.state.opacity.interpolate({
+      inputRange: [0, 100],
+      outputRange: [fg, bg]
+    });
+
+    return (
+      <TouchableWithoutFeedback onPress={this.toggle}>
+        <Animated.View style={[styles.circle, {backgroundColor: color}]}>
+          <Ionicon style={styles.checkmark} name="ios-checkmark"/>
+        </Animated.View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+class Radiobox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: props.selected,
+      size: new Animated.Value(props.selected ? size-gap*2-border*2 : 0)
+    };
+  }
+
+  toggle = () => {
+    this.setState({selected: !this.state.selected});
+    Animated.timing(this.state.size, {
+      toValue: this.state.selected ? size-gap*2-border*2 : 0,
+      duration: duration
+    }).start();
   }
 
   render() {
     return (
-      <ScrollView style={{ flex: 1, padding: 20 }}>
-        <Text style={{ color: '#1b73b4', fontSize: 24 }}>
-          My Hellos ({store.list.length})
-        </Text>
+      <TouchableWithoutFeedback onPress={this.toggle}>
+        <View style={styles.circle}>
+          <Animated.View style={[styles.dot, {
+            width: this.state.size,
+            height: this.state.size,
+            borderRadius: Animated.divide(this.state.size, 2),
+          }]}>
+          </Animated.View>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
 
-        {store.list.map((item, key) => (
-          <Text key={`item-${key}`} style={{ color: '#444' }}>
-            {item}
-          </Text>
-        ))}
+@observer
+class App extends React.Component {
+  render() {
+    return (
+      <Container>
+          <Header>
+            <Button transparent>
+              <Icon name='ios-menu'/>
+            </Button>
+            <Title>Header</Title>
+          </Header>
 
-        <TouchableOpacity onPress={() => store.add('Hello!')}>
-          <Text style={{ color: '#1b73b4' }}>
-            sayHello()
-          </Text>
-        </TouchableOpacity>
-      </ScrollView>
+          <Content>
+            <Text>Text</Text>
+            <View style={{flex: 1, flexDirection: 'row'}}><Checkbox/><Text style={{margin: 10}}>Hello</Text></View>
+            <View style={{flex: 1, flexDirection: 'row'}}><Radiobox/><Text>World</Text></View>
+          </Content>
+
+          <Footer>
+              <FooterTab>
+                  <Button>
+                    Map
+                    <Icon name='ios-map' />
+                  </Button>
+                  <Button active>
+                    Search
+                    <Icon name='ios-search' />
+                  </Button>
+                  <Button>
+                    Call
+                    <Icon name='ios-call' />
+                  </Button>
+              </FooterTab>
+          </Footer>
+      </Container>
     );
   }
 }
